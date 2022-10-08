c0c1e50444d47a3ae4bcd3e13dd44660f5da1893
One <494231599@qq.com>
2022-10-08 19:45:42 +0800

    提交修改后的文件

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ App({
       let navBarHeight = 44；
       let navBarHeightNew = navBarHeight * 750 / wx.getSystemInfoSync().windowWidth;
     */
-
-
+   
     // 设置系统全局对象保存信息
     gloableSystemMessage(this);
     
